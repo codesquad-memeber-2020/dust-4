cc55fcde68aa84e267c5e2fd354489b62216ec98
limwin94 <amuguna94@gmail.com>
2020-03-30 15:56:48 +0900

    [#1] feat: 탭바 컨트롤러 생성 (프로젝트 생성)

---
+++ iOS/Dust/Dust/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Dust
+//
+//  Created by 임승혁 on 2020/03/30.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

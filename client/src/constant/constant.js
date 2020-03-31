9d161704bcee8489ea8b3eaf94a14687ada535c5
baekCode <sosaer88@gmail.com>
2020-03-31 22:04:34 +0900

    feat: 미세먼지 함수 분기처리

---
+++ client/src/constant/constant.js
@@ -0,0 +1,12 @@
+const DEFAULT_STATION = '종로구';
+
+const DUST_GARADE = {
+  STATUS_MESSAGE: {
+    GOOD: '좋음',
+    NORMAL: '보통',
+    BAD: '나쁨',
+    VERY_BAD: '매우나쁨'
+  }
+};
+
+export { DEFAULT_STATION, DUST_GARADE };
--- client/src/js/dust/index.js
+++ client/src/js/dust/index.js
@@ -1,21 +1,70 @@
 import { MOCK_DATA } from '../../data/mock.js';
+import { DEFAULT_STATION, DUST_GARADE } from '../../constant/constant.js';
 import { $SELETOR, $SELETOR_ALL } from '../../util/index.js';
-import {TabNav} from '../../util/tabNav.js';
+import { TabNav } from '../../util/tabNav.js';
 
 const DUST_STATION = MOCK_DATA.DUST_STATION;
 const DUST_TIMELINE = MOCK_DATA.DUST_TIMELINE;
-const dustElement = $SELETOR('#dust');
-const statusStationElement = $SELETOR('.status-station strong');
-const timelineElement = $SELETOR('.dust-timeline');
-const timelineRender = DUST_TIMELINE.reduce((list, item) => {
-  list += `<li><span class="dust-grade${item.pm10Grade}" style="width:${item.pm10Value / 2}%">${item.pm10Value}</span></li>`;
+
+const DUST_ELEMENT = {
+  wrap: $SELETOR('#dust'),
+  emoji: $SELETOR('.status-img'),
+  station: $SELETOR('.status-station strong'),
+  statusTxt: $SELETOR('.status-txt'),
+  value: $SELETOR('.display-value span'),
+  time: $SELETOR('.display-time span'),
+  timeline: $SELETOR('.dust-timeline')
+};
+
+const timelineList = DUST_TIMELINE.reduce((list, item) => {
+  const dustGrade = item.pm10Grade;
+  const dustValue = item.pm10Value;
+  const dustProgressWidth = dustValue / 2;
+  list += `<li><span class="dust-grade${dustGrade}" style="width:${dustProgressWidth}%">${dustValue}</span></li>`;
   return list;
 }, '');
-statusStationElement.innerHTML = DUST_STATION || '종로';
-timelineElement.insertAdjacentHTML('beforeend', timelineRender);
-
 
 const tabNav = new TabNav({
-  tabNavItems : $SELETOR_ALL('.tab-nav li'),
-  tabContents : $SELETOR_ALL('.tab-contents section')
-})
+  tabNavItems: $SELETOR_ALL('.tab-nav li'),
+  tabContents: $SELETOR_ALL('.tab-contents section')
+});
+
+const getDustStatusText = grade => {
+  switch (grade) {
+    case '1':
+      return DUST_GARADE.STATUS_MESSAGE.GOOD;
+    case '2':
+      return DUST_GARADE.STATUS_MESSAGE.NORMAL;
+    case '3':
+      return DUST_GARADE.STATUS_MESSAGE.BAD;
+    case '4':
+      return DUST_GARADE.STATUS_MESSAGE.VERY_BAD;
+  }
+};
+
+const updateDustGradeView = ({ target, station, grade, statusText }) => {
+  return (
+    target.wrap.classList.add(`grade-${grade}`),
+    (target.station.innerHTML = station || DEFAULT_STATION),
+    (target.emoji.innerHTML = `<img src="../../assets/emoji/grade_${grade}.png" alt="${statusText}">`)
+  );
+};
+
+const updateDustTimelineView = ({ target, list }) => {
+  return target.timeline.insertAdjacentHTML('beforeend', list);
+};
+
+const dustViewInit = () => {
+  updateDustGradeView({
+    target: DUST_ELEMENT,
+    station: DUST_STATION,
+    grade: DUST_TIMELINE[0].pm10Grade,
+    statusText: getDustStatusText(DUST_TIMELINE[0].pm10Grade)
+  });
+  updateDustTimelineView({
+    target: DUST_ELEMENT,
+    list: timelineList
+  });
+};
+
+dustViewInit();

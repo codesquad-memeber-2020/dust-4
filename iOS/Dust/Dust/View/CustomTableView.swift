8935ae4e0af9ff08f0fca8c244545d59431015f7
limwin94 <amuguna94@gmail.com>
2020-03-31 12:49:23 +0900

    [#3] feat: custom view class 추가

---
+++ iOS/Dust/Dust/View/CustomTableView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableView.swift
+//  Dust
+//
+//  Created by 임승혁 on 2020/03/31.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableView: UITableView {
+
+}

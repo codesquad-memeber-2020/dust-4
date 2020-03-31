4c00b4a581b3a0d4f534288e270075b9e381c7e9
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-03-31 16:24:29 +0900

    chore: DustStatusQuo에 불필요한 location 필드값 제거

--- BE/src/main/java/com/codesquad/dust4/domain/DustStatusQuo.java
+++ BE/src/main/java/com/codesquad/dust4/domain/DustStatusQuo.java
@@ -3,16 +3,14 @@ package com.codesquad.dust4.domain;
 public class DustStatusQuo {
 
   private String dataTime; //측정일
-  private LocationOfStation location; //측정소 정보
   private String pm10Value; //미세먼지(PM10) 농도
   private String pm10Grade; //미세먼지(PM10) 24시간 등급
 
   public DustStatusQuo() {}
 
-  public DustStatusQuo(String dataTime, LocationOfStation location, String pm10Value,
+  public DustStatusQuo(String dataTime, String pm10Value,
       String pm10Value24, String pm10Grade, String pm10Grade1h) {
     this.dataTime = dataTime;
-    this.location = location;
     this.pm10Value = pm10Value;
     this.pm10Grade = pm10Grade;
   }
@@ -23,14 +21,6 @@ public class DustStatusQuo {
 
   public void setDataTime(String dataTime) {
     this.dataTime = dataTime;
-  }
-
-  public LocationOfStation getLocation() {
-    return location;
-  }
-
-  public void setLocation(LocationOfStation location) {
-    this.location = location;
   }
 
   public String getPm10Value() {

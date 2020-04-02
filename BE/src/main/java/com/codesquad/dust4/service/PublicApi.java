e91a068a963558a0580515abc90c48ff24ed8119
koo bohyeon <goosee115@gmail.com>
2020-04-02 17:12:19 +0900

    Feature: forecast api 작업
    예보 객체 넘겨주는 작업 함
    api-key가 다른 클래스에서 중복되어 사용되서 클래스로 분리함
    적절한 값이 오지 않을 경우의 예외처리는 아직 하지 못함!!

---
+++ BE/src/main/java/com/codesquad/dust4/service/PublicApi.java
@@ -0,0 +1,6 @@
+package com.codesquad.dust4.service;
+
+public class PublicApi {
+    public static final String API_KEY = "GHE6PYOY0ZzdJ8j%2BISHC3VOKNQI1DG60chkCAE19uUD5s1XCnXOseXvJfBJkFZlI1zpn2EGLDnkYy%2FQrgLpd6A%3D%3D";
+    public static final String API_URL = "http://openapi.airkorea.or.kr/openapi/services/rest/";
+}

b36cc4df883b8ea95f1ae3ae5dbab7a740a40cfe
baekCode <sosaer88@gmail.com>
2020-04-03 11:20:26 +0900

    feat: 예보 api

--- client/src/js/dustForecast/index.js
+++ client/src/js/dustForecast/index.js
@@ -4,23 +4,37 @@ $GET(`http://13.124.46.74:8080/forecast`).then(data => {
   localStorage.setItem('DUST_FORECAST', JSON.stringify(data));
 });
 
-const dustData = localStorage.getItem('DUST_FORECAST');
-const dustDataParse = JSON.parse(dustData);
-const img = dustDataParse.content.images;
+const FORECAST_DATA = {
+  grade: null,
+  overall: null,
+  img: null
+};
 
 const FORECAST_ELEMENT = {
   display: $SELETOR('.info-display img'),
   slider: $SELETOR('.slider'),
-  btn: $SELETOR('#btn')
+  btn: $SELETOR('#btn'),
+  grade: $SELETOR('.info-grade'),
+  overall: $SELETOR('.info-overall')
 };
 
-const init = () => {};
+const init = () => {
+  const dustData = localStorage.getItem('DUST_FORECAST');
+  const dustDataParse = JSON.parse(dustData);
+  FORECAST_DATA.grade = dustDataParse.content.informGrade;
+  FORECAST_DATA.overall = dustDataParse.content.informOverall;
+  FORECAST_DATA.img = dustDataParse.content.images;
+
+  FORECAST_ELEMENT.grade.innerHTML = FORECAST_DATA.grade;
+  FORECAST_ELEMENT.overall.innerHTML = FORECAST_DATA.overall;
+};
+
 
 FORECAST_ELEMENT.slider.addEventListener('input', e => {
   const value = parseInt(e.target.value);
-  const step = 100 / img.length;
+  const step = 100 / FORECAST_DATA.img.length;
   if (value === 100) return;
-  const printImg = (FORECAST_ELEMENT.display.src = img[parseInt(value / step)]);
+  const printImg = (FORECAST_ELEMENT.display.src = FORECAST_DATA.img[parseInt(value / step)]);
   return printImg;
 });
 
